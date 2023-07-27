af078ff8fbaceb4cd72b53b4bca40e80bddcdd9c
samishal1998 <samishal.1998@gmail.com>
2023-07-27 15:57:10 +0300

    Enhanced `OptionalApiProperty` Decorator to include `class-validator::IsOptional`

--- src/openapi/decorators/index.ts
+++ src/openapi/decorators/index.ts
@@ -1,8 +1,15 @@
 import { ApiProperty, ApiPropertyOptions } from '@nestjs/swagger';
+import { applyDecorators } from '@nestjs/common';
+import { IsOptional } from 'class-validator';
 
 export const OptionalApiProperty = (
-	options?: Omit<ApiPropertyOptions, 'required'>,
-) => ApiProperty({ ...(options ?? {}), required: false });
+	apiPropertyOptions?: Omit<ApiPropertyOptions, 'required'>,
+	isOptionalOptions?: Parameters<typeof IsOptional>[0],
+) =>
+	applyDecorators(
+		IsOptional(isOptionalOptions),
+		ApiProperty({ ...(apiPropertyOptions ?? {}), required: false }),
+	);
 
 export const DateApiProperty = (options?: Omit<ApiPropertyOptions, 'type'>) =>
 	ApiProperty({ ...(options ?? {}), type: Date });
