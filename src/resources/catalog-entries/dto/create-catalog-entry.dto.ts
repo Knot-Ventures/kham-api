94efb7368fa8185cd97e17656ad9bb22e4902b95
yousefwahba <yousefwahba.official@gmail.com>
2023-08-03 11:45:26 +0300

    add-catalog-entries-dtos

--- src/resources/catalog-entries/dto/create-catalog-entry.dto.ts
+++ src/resources/catalog-entries/dto/create-catalog-entry.dto.ts
@@ -1,1 +1,55 @@
-export class CreateCatalogEntryDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ProductEntity } from '../entities/product.entity';
+import { VendorEntity } from '../entities/vendor.entity';
+
+export class CreateCatalogEntryDto {
+	@IsString()
+	@ApiProperty()
+	productId: string;
+
+	@IsString()
+	@ApiProperty()
+	vendorId: string;
+
+	@IsString()
+	@ApiProperty()
+	description: string;
+
+	@IsArray()
+	@IsString({ each: true })
+	@ApiProperty({ type: [String] })
+	images: string[];
+
+	@IsString()
+	@ApiProperty()
+	title: string;
+
+	@IsString()
+	@ApiProperty()
+	subtitle: string;
+
+	@IsNumber()
+	@ApiProperty()
+	min_qty: number;
+
+	@IsNumber()
+	@ApiProperty()
+	available_qty: number;
+
+	@IsString()
+	@ApiProperty()
+	unit: string;
+
+	@IsNumber()
+	@ApiProperty()
+	average_market_price: number;
+
+	@IsOptional()
+	@ApiPropertyOptional({ type: () => ProductEntity })
+	product?: ProductEntity;
+
+	@IsOptional()
+	@ApiPropertyOptional({ type: () => VendorEntity })
+	vendor?: VendorEntity;
+}
--- src/resources/catalog-entries/dto/update-catalog-entry.dto.ts
+++ src/resources/catalog-entries/dto/update-catalog-entry.dto.ts
@@ -1,4 +1,53 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCatalogEntryDto } from './create-catalog-entry.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsArray, IsNumber, IsString } from 'class-validator';
+import { ProductEntity } from '../entities/product.entity';
+import { VendorEntity } from '../entities/vendor.entity';
 
-export class UpdateCatalogEntryDto extends PartialType(CreateCatalogEntryDto) {}
+export class UpdateCatalogEntryDto {
+	@IsString()
+	@ApiPropertyOptional()
+	productId?: string;
+
+	@IsString()
+	@ApiPropertyOptional()
+	vendorId?: string;
+
+	@IsString()
+	@ApiPropertyOptional()
+	description?: string;
+
+	@IsArray()
+	@IsString({ each: true })
+	@ApiPropertyOptional({ type: [String] })
+	images?: string[];
+
+	@IsString()
+	@ApiPropertyOptional()
+	title?: string;
+
+	@IsString()
+	@ApiPropertyOptional()
+	subtitle?: string;
+
+	@IsNumber()
+	@ApiPropertyOptional()
+	min_qty?: number;
+
+	@IsNumber()
+	@ApiPropertyOptional()
+	available_qty?: number;
+
+	@IsString()
+	@ApiPropertyOptional()
+	unit?: string;
+
+	@IsNumber()
+	@ApiPropertyOptional()
+	average_market_price?: number;
+
+	@ApiPropertyOptional({ type: () => ProductEntity })
+	product?: ProductEntity;
+
+	@ApiPropertyOptional({ type: () => VendorEntity })
+	vendor?: VendorEntity;
+}
